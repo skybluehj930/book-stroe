de3e849d459d4e19faee419360802116baa41369
haeju lee <skybluehj930@gmail.com>
2023-01-01 23:58:56 +0900

    clean up unused sources

--- src/test/java/com/lhj/bookstore/service/BookStoreServiceTest.java
+++ src/test/java/com/lhj/bookstore/service/BookStoreServiceTest.java
@@ -1,7 +1,7 @@
 package com.lhj.bookstore.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,11 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import com.lhj.bookstore.entity.BookInfoEntity;
 import com.lhj.bookstore.repository.BookInfoRepository;
